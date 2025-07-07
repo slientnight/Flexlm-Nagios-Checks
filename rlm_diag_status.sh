9637d82f4db0bf9512578ea07fe81b65e92d05e3
Marshall Hollis <russiansharpshot@gmail.com>
2025-07-07 09:42:03 -0400

    Add files via upload

---
+++ rlm_diag_status.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+rlm=/usr/local/nagios/libexec/rlmutil
+port=$1
+server=$2
+isv=$3
+get_diag(){
+DIAG_STATUS=`$rlm rlmstat -a -i ${isv} -c ${port}@${server} | grep -oh "server status on"`
+if [[ "$DIAG_STATUS" == "server status on" ]]; then
+    echo "OK - $isv can be checked out"
+     exit 0
+else
+    echo "Critical - $isv can't be checked out"
+     exit 2
+fi
+}
+get_diag
+
